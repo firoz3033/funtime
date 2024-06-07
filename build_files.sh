f5b70e793a27f356d52aa8435ee326486bc241f7
firoz3033 <firozansari3285@gmail.com>
2024-06-07 11:56:01 +0530

    error fix 2

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 # Install dependencies
-pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Collect static files
-python3.9 manage.py collectstatic 
+python3.9 manage.py collectstatic --noinput
