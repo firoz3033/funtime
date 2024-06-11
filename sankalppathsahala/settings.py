cf7f8d0d25d2bb76affc77cfee3051b8ef7fd177
firoz3033 <firozansari3285@gmail.com>
2024-06-11 16:45:04 +0530

    media error

--- sankalppathsahala/settings.py
+++ sankalppathsahala/settings.py
@@ -30,8 +30,9 @@ SECRET_KEY = os.getenv('Secretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+# ALLOWED_HOSTS = []
 
 
 # Application definition
